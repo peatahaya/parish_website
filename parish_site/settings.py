acb786e85d5621eb106599a3ca09f920cf313df9
peatahaya <pantherbielicki2@vp.pl>
2025-08-28 19:33:19 +0200

    Add Render domain to ALLOWED_HOSTS

--- parish_site/settings.py
+++ parish_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0y=a517#e^gisxon!)sg@=s2*ry@rvofz%i8h424c^c=a)7ib3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "parish-website.onrender.com"]
 
 
 # Application definition
