66d0024a38151589f0eac4862d2173d37701c1f9
peatahaya <pantherbielicki2@vp.pl>
2025-08-28 19:50:31 +0200

    serving statics

--- cms/urls.py
+++ cms/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
